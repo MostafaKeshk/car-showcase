a4a449a7b399097b3e68cdb4c4b633e24057ba60
mostafa <mostafa_mohamed12326@yahoo.com>
2023-06-15 21:38:11 +0300

    first commit

---
+++ components/CustomButton.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+const CustomButton = () => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className="custom-btn"
+      onClick={() => {}}
+    >
+      Explore Cars
+    </button>
+  );
+};
+
+export default CustomButton;
